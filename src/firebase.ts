60ce472af8250d61c8a9d16d8fdfce19043516e1
Onaefe <onaefe@onaxsys.com>
2022-06-27 11:57:13 +0100

    clean up

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,6 @@
 import { ServiceAccount } from 'firebase-admin';
 import {initializeApp,cert} from 'firebase-admin/app';
 import {getFirestore} from 'firebase-admin/firestore';
-import serviceAccount from './serviceAcctKey.json';
 import ServiceAcct from './serviceAccountKey.json';
 
 
